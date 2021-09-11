9395f48fb739370ed47d9b6373c2202db3768b32
Nikhil <nikhil@example.com>
2021-09-11 11:30:19 +0530

    coomit

--- test/fixtures/wdio.conf/wdio-multi-browser.conf.js
+++
@@ -1,9 +0,0 @@
-var baseConfig = require('./wdio.conf.js').getConfig()
-
-baseConfig.capabilities = [{
-    browserName: 'phantomjs'
-}, {
-    browserName: 'chrome'
-}]
-
-exports.config = baseConfig
