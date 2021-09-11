09b4455f76e9d3292d18391a15a24afb5ccb690e
Nikhil <nikhil@example.com>
2021-09-11 10:42:34 +0530

    Changed allure reports parameters

---
+++ test/fixtures/wdio.conf/wdio.conf.js
@@ -0,0 +1,25 @@
+var allureReporter = require('../../../build/reporter')
+allureReporter.reporterName = 'allure'
+
+exports.getConfig = function () {
+    return {
+        baseUrl: 'http://localhost:54392',
+        coloredLogs: true,
+        logLevel: 'silent',
+        services: ['phantomjs'],
+        reporters: [allureReporter],
+        framework: 'mocha',
+
+        reporterOptions: {
+            allure: {
+                outputDir: '.allure-results'
+            }
+        },
+        sync: false,
+        screenshotPath: './screenshots',
+        capabilities: [{
+            browserName: 'phantomjs',
+            deviceName : 'NewDevice'
+        }]
+    }
+}
